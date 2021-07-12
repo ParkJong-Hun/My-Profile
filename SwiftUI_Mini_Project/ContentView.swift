895a7ad9c8ed460dfb5cbe6fcc7a41eeeb1a8483
ParkJong-Hun <thesn515@gmail.com>
2021-07-12 15:08:21 +0900

    2021/07/12 15:08 Start Project

---
+++ SwiftUI_Mini_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Mini_Project
+//
+//  Created by 박종훈 on 2021/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Mini_Project/SwiftUI_Mini_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Mini_ProjectApp.swift
+//  SwiftUI_Mini_Project
+//
+//  Created by 박종훈 on 2021/07/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Mini_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
